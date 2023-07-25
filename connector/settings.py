631c678f33898aa67b58493d773cc26d4bc50632
Tejesh Sao <Tejeshsao18@gmail.com>
2023-07-25 11:07:43 +0530

    -commit
    Mysql- dbsqlite

--- connector/settings.py
+++ connector/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'connector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'connector',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306',
+#         'USER':'root',
+#         'PASSWORD':'Tejesh123',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'connector',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':'Tejesh123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
